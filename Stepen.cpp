2b533dd54b05c0a4c4744c7dafb067f2c9842c8d
InvisEA <Geniusser@mail.ru>
2014-02-25 05:00:55 -0800

    Update Stepen.cpp

--- Stepen.cpp
+++ Stepen.cpp
@@ -10,11 +10,15 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	int n;
 	int i=2;
+	is_prime=true;
 	cin >> n;
 	while (i<=sqrt(static_cast<double>(n)))
 	{
 		if (n % i == 0)
+		{	
 			cout << "Number is not prime";
+			is_prime=false;
+		}
 		i++;
 	}
 	return 0;
